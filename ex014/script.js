667a53e7df1e32c004b2ef2bb70ea9d2b07e2039
gleissonbh <71742662+gleissonbh@users.noreply.github.com>
2021-09-29 13:07:28 -0300

    Initial commit

---
+++ ex014/script.js
@@ -0,0 +1,7 @@
+function carregar()
+var hora = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+
+msg.innerHTML = `Agora são ${hora} horas.`
