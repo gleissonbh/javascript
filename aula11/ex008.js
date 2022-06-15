950450d819e1b7e499ec50f901eac94dd3b59de2
gleissonbh <71742662+gleissonbh@users.noreply.github.com>
2022-06-14 22:13:12 -0300

    atualizei a tabuada

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -4,3 +4,4 @@ if(vel >= 60){
     console.log('Você ultrapassou a velocidade permitida. MULTADO!')
 }
 console.log('Dirija sempre usando cinto de segurança')
+
--- ex014/script.js
+++ ex014/script.js
@@ -1,7 +1,22 @@
-function carregar()
-var hora = window.document.getElementById('msg')
-var img = window.document.getElementById('imagem')
-var data = new Date()
-var hora = data.getHours()
+function carregar(){
+    var hora = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
 
-msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        img.src='fotomanha.png.png'
+        document.body.style.backgroundColor = '#EEDAB5'
+    }else if(hora >= 12 && hora <18){
+        img.src= 'fototarde.png.png'
+        document.body.style.backgroundColor = '#F28749'
+    }else{
+        img.src = 'fotonoite.png.png'
+        document.body.style.backgroundColor = '#484F55'
+    }
+    
+}
+
+
+
